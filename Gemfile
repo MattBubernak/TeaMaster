b9453fb261e7de54c350fcf4c8a37016bc6d732a
Matt Bubernak <matt.bubernak@gmail.com>
2017-01-31 22:54:43 -0700

    fixed materialize + basic nav

--- Gemfile
+++ Gemfile
@@ -24,6 +24,8 @@ gem 'erb2haml'
 
 gem 'materialize-sass'
 
+gem 'sprockets-rails', :require => 'sprockets/railtie'
+
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,7 @@ module TeaMaster
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.assets.paths << Rails.root.join("vendor", "assets", "images")
+    config.assets.paths << Rails.root.join("vendor", "assets")
+
   end
 end
