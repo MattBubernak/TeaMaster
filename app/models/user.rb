ad50f741cd5c8dc7c70384f6236f1000f4f5563b
Matt Bubernak <matt.bubernak@gmail.com>
2017-03-06 21:20:12 -0700

    some basic layout stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   acts_as_authentic
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
   enum gender: [ :male, :female]
+  has_many :recipes
   def is_admin?
     admin
   end
