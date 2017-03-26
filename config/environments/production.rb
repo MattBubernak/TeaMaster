a8afa39159a6e1e8ed5e944c5210b5f9ca5c8d30
Matt Bubernak <matt.bubernak@gmail.com>
2017-03-26 16:02:02 -0600

    capistrano

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,7 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  server '54-202-105-198', user: 'deploy', roles: %w{web app db}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
