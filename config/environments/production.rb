05a3bacb01911709e9873fe3f1e47cd383c45e3d
Matt Bubernak <matt.bubernak@gmail.com>
2017-03-26 16:40:06 -0600

    fixed production server command

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
+server '54-202-105-198', user: 'deploy', roles: %w{web app db}
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  server '54-202-105-198', user: 'deploy', roles: %w{web app db}
   # Code is not reloaded between requests.
   config.cache_classes = true
 
