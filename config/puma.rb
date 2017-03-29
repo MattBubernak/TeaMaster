fcf0ea1d3a07a665b469a3c24e932fcefba4857c
Matt Bubernak <matt.bubernak@gmail.com>
2017-03-28 20:13:40 -0600

    another puma revision

--- config/puma.rb
+++ config/puma.rb
@@ -2,14 +2,20 @@
 
 # `shared_dir` is the symlinked `shared/` directory created
 # by Capistrano - `/home/deploy/my_app/shared`
-File.open("unix://#{shared_dir}/tmp/yolo.txt", "w") {}
+workers 2
+threads 1, 6
+app_path = File.expand_path("../..", __FILE__)
+shared_path = "#{app_path}/shared"
+rails_env = ENV['RAILS_ENV'] || "production"
+environment rails_env
+File.open("foo.txt", "w") {}
 # Set up socket location
-bind "unix://#{shared_dir}/tmp/sockets/puma.sock"
+bind "unix://#{shared_path}/tmp/sockets/puma.sock"
 
 # Logging
-stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+stdout_redirect "#{shared_path}/log/puma.stdout.log", "#{shared_path}/log/puma.stderr.log", true
 
 # Set master PID and state locations
-pidfile "#{shared_dir}/tmp/pids/puma.pid"
-state_path "#{shared_dir}/tmp/pids/puma.state"
+pidfile "#{shared_path}/tmp/pids/puma.pid"
+state_path "#{shared_path}/tmp/pids/puma.state"
 activate_control_app
