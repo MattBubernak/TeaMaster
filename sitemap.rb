bc29529226ea06168c199e319ad611b24d4ef197
Matt Bubernak <matt.bubernak@gmail.com>
2017-04-29 14:31:13 -0600

    fixes for ckeditor

--- sitemap.rb
+++ sitemap.rb
@@ -5,6 +5,7 @@ SitemapGenerator::Sitemap.default_host = 'http://www.HerbalTeaMaster.com'
 SitemapGenerator::Sitemap.create do
   add '/welcome/index', :changefreq => 'daily', :priority => 0.9
   add '/recipes', :changefreq => 'daily'
+  add '/blog', :changefreq => 'daily'
   add '/recipes/1', :changefreq => 'daily'
   add '/recipes/14', :changefreq => 'daily'
   add '/ingredients', :changefreq => 'daily'
